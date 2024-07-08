c48796ab523342944d5601e83921afc27e67ca3b
ohnoitsaninja <ohnoitsaninja@gmail.com>
2024-07-08 04:34:25 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,34 @@
-function setup() {
-    createCanvas(400, 400);
+let img1, img2;
+let shaderProgram;
+let canvasContainer;
+
+function preload() {
+  shaderProgram = loadShader('blend.vert', 'blend.frag');
 }
 
-function draw() {
-    background(220);
-    // Your drawing code
+function setup() {
+  noCanvas();
+  
+  const fileInput1 = select('#fileInput1');
+  const fileInput2 = select('#fileInput2');
+  const blendButton = select('#blendButton');
+  canvasContainer = select('#canvasContainer');
+
+  fileInput1.changed(() => loadImage(URL.createObjectURL(fileInput1.elt.files[0]), img => img1 = img));
+  fileInput2.changed(() => loadImage(URL.createObjectURL(fileInput2.elt.files[0]), img => img2 = img));
+
+  blendButton.mousePressed(() => {
+    if (img1 && img2) {
+      const cnv = createCanvas(img1.width, img1.height, WEBGL);
+      cnv.parent('canvasContainer');
+      blendImages();
+    }
+  });
+}
+
+function blendImages() {
+  shader(shaderProgram);
+  shaderProgram.setUniform('texture1', img1);
+  shaderProgram.setUniform('texture2', img2);
+  rect(0, 0, width, height);
 }
